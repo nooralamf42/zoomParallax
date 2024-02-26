c82a4b6aa89ba4027ce5ef11874edb86a6c2124d
nooralamf42 <nooralamf42@gmail.com>
2024-02-26 15:43:06 +0530

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useScroll, useTransform, motion } from "framer-motion";
 import React, { useEffect, useRef } from "react";
 import Lenis from "@studio-freight/lenis";
+import axios from "axios";
 
 function App() {
   useEffect(() => {
@@ -27,7 +28,7 @@ function App() {
   const scale6 = useTransform(scrollYProgress, [0, 1], [1, 6]);
   const scale7 = useTransform(scrollYProgress, [0, 1], [1, 7]);
 
-  const round = useTransform(scrollYProgress, [0, 1], [90, 1])
+  const round = useTransform(scrollYProgress, [0, 1], [90, 1]);
   const images = [
     {
       src: "https://plus.unsplash.com/premium_photo-1670445045282-36648e89af6b?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -67,31 +68,92 @@ function App() {
     },
   ];
 
+  const paymentHandler = async () => {
+    const {data: { id, amount }} = await axios.post('https://api.razorpay.com/v1/orders', {
+      "amount" : 50000,
+      "currency" : "INR",
+      "receipt" : "testfjkdjsdlfk"
+    },{
+      headers: {
+        'Authorization': 'Basicrzp_test_xOKEYArzgTHRZ0:SnrZSdP6XgzhKZfTaVyOeIhV',
+        "Content-Type": "application/json",
+      },
+    })
+
+
+    console.log(id)
+    let options = {
+      key: "rzp_test_xOKEYArzgTHRZ0", // Enter the Key ID generated from the Dashboard
+      amount: amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+      currency: "INR",
+      name: "Acme Corp",
+      description: "Test Transaction",
+      image: "https://example.com/your_logo",
+      order_id: id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+      handler: function (response) {
+        console.log(response)
+      },
+      prefill: {
+        name: "Gaurav Kumar",
+        email: "gaurav.kumar@example.com",
+        contact: "9000090000",
+      },
+      notes: {
+        address: "Razorpay Corporate Office",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
+    var razor = new window.Razorpay(options);
+    razor.on("payment.failed", function (response) {
+      alert(response.error.code);
+      alert(response.error.description);
+      alert(response.error.source);
+      alert(response.error.step);
+      alert(response.error.reason);
+      alert(response.error.metadata.order_id);
+      alert(response.error.metadata.payment_id);
+    });
+
+    razor.open()
+  };
+
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <button
+        className="p-2 border bg-green-400"
+        onClick={() => paymentHandler()}
+      >
+        pay here
+      </button>
+    </div>
+  );
   return (
     <>
-    <div className="h-[25vh] sm:h-[50vh] box-border flex justify-center items-center">
-      <h1 className="text-[10vw] font-semibold">Scroll to begin</h1>
-    </div>
-    <div ref={container} className="relative h-[300vh]">
-      <div className="h-screen sticky top-0 overflow-hidden">
-        {images.map((image, index) => (
-          <motion.div
-            style={{ scale: image.scale , rotate: round}}
-            key={index}
-            className="flex justify-center items-center h-full w-full absolute top-0"
-          >
-            <div className={`absolute ${image.className}`}>
-              <img
-                className="object-cover w-full h-full"
-                src={image.src}
-                alt=""
-                srcset=""
-              />
-            </div>
-          </motion.div>
-        ))}
+      <div className="h-[25vh] sm:h-[50vh] box-border flex justify-center items-center">
+        <h1 className="text-[10vw] font-semibold">Scroll to begin</h1>
       </div>
-    </div>
+      <div ref={container} className="relative h-[300vh]">
+        <div className="h-screen sticky top-0 overflow-hidden">
+          {images.map((image, index) => (
+            <motion.div
+              style={{ scale: image.scale, rotate: round }}
+              key={index}
+              className="flex justify-center items-center h-full w-full absolute top-0"
+            >
+              <div className={`absolute ${image.className}`}>
+                <img
+                  className="object-cover w-full h-full"
+                  src={image.src}
+                  alt=""
+                  srcset=""
+                />
+              </div>
+            </motion.div>
+          ))}
+        </div>
+      </div>
       <div className="h-[100vh] flex justify-center items-center">
         <h1 className="text-[10vw] font">was it smooth?</h1>
       </div>
